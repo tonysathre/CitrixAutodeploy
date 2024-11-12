db6c8c5434fb6bf037adfd992b68952fb84848a7
tonysathre <tony@ghostbit.org>
2024-11-12 14:45:38 -0600

    fix syntax error

--- tests/Initialize-CtxAutodeployEnv.Tests.ps1
+++ tests/Initialize-CtxAutodeployEnv.Tests.ps1
@@ -8,41 +8,38 @@ Describe 'Initialize-CtxAutodeployEnv' {
         Mock Write-VerboseLog {}
     }
 
-    Context 'When called' {
-        It 'Should import the required modules without errors' {
-            Mock Import-Module { return $true } -ModuleName 'CitrixAutodeploy'
-            $Modules = @(
-                'Citrix.ADIdentity.Commands',
-                'Citrix.Broker.Commands',
-                'Citrix.ConfigurationLogging.Commands',
-                'Citrix.MachineCreation.Commands'
-            )
 
-            foreach ($Module in $Modules) {
-                Remove-Module -Name $Module -ErrorAction SilentlyContinue
-            }
+    It 'Should import the required modules without errors' {
+        Mock Import-Module { return $true } -ModuleName 'CitrixAutodeploy'
+        $Modules = @(
+            'Citrix.ADIdentity.Commands',
+            'Citrix.Broker.Commands',
+            'Citrix.ConfigurationLogging.Commands',
+            'Citrix.MachineCreation.Commands'
+        )
 
-            { Initialize-CtxAutodeployEnv } | Should -Not -Throw
+        foreach ($Module in $Modules) {
+            Remove-Module -Name $Module -ErrorAction SilentlyContinue
         }
 
+        { Initialize-CtxAutodeployEnv } | Should -Not -Throw
+    }
+
+    It 'Should throw an error if a module fails to import' {
+        Mock Import-Module { return $true } -ModuleName 'CitrixAutodeploy'
+        $InvalidModule = 'NonExistent.Module'
+        $Modules = @(
+            'Citrix.ADIdentity.Commands',
+            'Citrix.Broker.Commands',
+            'Citrix.ConfigurationLogging.Commands',
+            'Citrix.MachineCreation.Commands',
+            $InvalidModule
+        )
+
+        foreach ($Module in $Modules) {
+            Remove-Module -Name $Module -ErrorAction SilentlyContinue
         }
 
-        It 'Should throw an error if a module fails to import' {
-            Mock Import-Module { return $true } -ModuleName 'CitrixAutodeploy'
-            $InvalidModule = 'NonExistent.Module'
-            $Modules = @(
-                'Citrix.ADIdentity.Commands',
-                'Citrix.Broker.Commands',
-                'Citrix.ConfigurationLogging.Commands',
-                'Citrix.MachineCreation.Commands',
-                $InvalidModule
-            )
-
-            foreach ($Module in $Modules) {
-                Remove-Module -Name $Module -ErrorAction SilentlyContinue
-            }
-
-            { Initialize-CtxAutodeployEnv } | Should -Throw
-        }
+        { Initialize-CtxAutodeployEnv } | Should -Throw
     }
 }
